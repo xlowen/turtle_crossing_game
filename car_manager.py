aad712df02f3b5cc1b333d7063795b6af888b117
Amilton Siqueira <amiltonvsiqueira@gmail.com>
2024-10-21 01:16:29 -0300

    Add cars and their movements

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color(COLORS[random.randint(0, 5)])
+        self.shape("square")
+        self.setpos(300, random.randint(-200, 200))
+        self.shapesize(1.0, 2.0, 0)
+        self.setheading(180)
+
+    def car_move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-
+cars = []
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -13,12 +13,20 @@ player = Player()
 screen.onkeypress(player.move_up, "w")
 scoreboard = Scoreboard()
 
-
 game_is_on = True
+counter = 0
 while game_is_on:
+
     screen.update()
     time.sleep(0.1)
+    if counter % 5 == 0:
+        car_manager = CarManager()
+        cars.append(car_manager)
+
+    for car in cars:
+        car.car_move()
 
     if player.ycor() == 280:
         player.player_reset()
         scoreboard.level_up()
+    counter += 1
